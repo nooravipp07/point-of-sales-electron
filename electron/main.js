41d6e2102df2216fd6f823e3be64b6262e166c26
nooravipp07 <nurapip.nurapip@gmail.com>
2026-04-29 00:40:38 +0700

    Penyesuaian pada catalog product

--- electron/main.js
+++ electron/main.js
@@ -1,34 +1,35 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-
+const iconPath = path.join(__dirname, '../assets/icon.png');
 const isDev = process.env.NODE_ENV === 'development';
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
+	const win = new BrowserWindow({
+		icon: iconPath,
+		width: 1200,
+		height: 800,
+		webPreferences: {
+		nodeIntegration: false,
+		contextIsolation: true,
+		preload: path.join(__dirname, 'preload.js'),
+		},
+	});
 
-  // Load Vite dev server in dev, or built files in production
-  if (isDev) {
-    win.loadURL('http://localhost:5173');
-    win.webContents.openDevTools();
-  } else {
-    win.loadFile(path.join(__dirname, '../dist/index.html'));
-  }
+	// Load Vite dev server in dev, or built files in production
+	if (isDev) {
+		win.loadURL('http://localhost:5173');
+		win.webContents.openDevTools();
+	} else {
+		win.loadFile(path.join(__dirname, '../dist/index.html'));
+	}
 }
 
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  	if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  	if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
--- electron/preload.js
+++ electron/preload.js
@@ -2,7 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 // Expose safe APIs to the renderer process
 contextBridge.exposeInMainWorld('electronAPI', {
-  sendMessage: (channel, data) => ipcRenderer.send(channel, data),
-  onMessage: (channel, callback) =>
-    ipcRenderer.on(channel, (event, ...args) => callback(...args)),
+	sendMessage: (channel, data) => ipcRenderer.send(channel, data),
+	onMessage: (channel, callback) =>
+		ipcRenderer.on(channel, (event, ...args) => callback(...args)),
 });
