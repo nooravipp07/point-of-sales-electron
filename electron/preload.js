e8cbdbc41477352a8fd4cf68f7a3c327497f43ee
nooravipp07 <nurapip.nurapip@gmail.com>
2026-04-28 22:39:26 +0700

    first commit

---
+++ electron/preload.js
@@ -0,0 +1,8 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// Expose safe APIs to the renderer process
+contextBridge.exposeInMainWorld('electronAPI', {
+  sendMessage: (channel, data) => ipcRenderer.send(channel, data),
+  onMessage: (channel, callback) =>
+    ipcRenderer.on(channel, (event, ...args) => callback(...args)),
+});
